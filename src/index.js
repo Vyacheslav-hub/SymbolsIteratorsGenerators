eee24650a12053b45e45a661d2becde13f0acd9c
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-04-30 19:09:24 +0300

    Добавил файл index.js и добавил файлы для билда

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {TeamIterator} from "./TeamIterator.js";
+import {TeamGenerator} from "./TeamGenerator.js";
+import {canIterate} from "./canIterate.js";
+
+new TeamIterator();
+new TeamGenerator()
+canIterate();
